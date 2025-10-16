7b9c480c6f2a62e7f11be9da4b67c31201b9c5eb
markoskatsi <markoskatsi05@gmail.com>
2025-10-16 16:44:23 +0100

    Initial commit -Basic GET endpoints for claims API

---
+++ app.js
@@ -0,0 +1,90 @@
+// Imports ---------------------------------
+import express from "express";
+import database from "./database.js";
+import cors from "cors";
+
+// Configure express app -------------------
+const app = express();
+
+// Configure middleware ---------------------
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(cors({ origin: "*" }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Controllers ------------------------------
+const read = async (selectSql) => {
+  try {
+    const [result] = await database.query(selectSql);
+    return result.length === 0
+      ? { isSuccess: false, result: null, message: "No record(s) found" }
+      : { isSuccess: true, result: result, message: "Record(s) recovered" };
+  } catch (error) {
+    return {
+      isSuccess: false,
+      result: null,
+      message: `Failed to execute query: ${error.message}`,
+    };
+  }
+};
+
+const buildClaimsSelectSql = (id, variant) => {
+  let sql = "";
+  const table =
+    "Claims INNER JOIN Users ON Claims.ClaimUserID=Users.UserID INNER JOIN Claimstatus ON Claims.ClaimClaimstatusID=Claimstatus.ClaimstatusID";
+  const fields = [
+    "ClaimID",
+    "ClaimTitle",
+    "ClaimDescription",
+    "ClaimPublished",
+    "ClaimUserID",
+    "CONCAT(Users.UserFirstname, ' ', Users.UserLastname) AS ClaimUserName",
+    "ClaimClaimstatusID",
+    "ClaimstatusName",
+  ];
+
+  switch (variant) {
+    case "users":
+      sql = `SELECT ${fields} FROM ${table} WHERE Claims.ClaimUserID = ${id}`;
+      break;
+    default:
+      sql = `SELECT ${fields} FROM ${table}`;
+      if (id) sql += ` WHERE ClaimID = ${id}`;
+  }
+
+  return sql;
+};
+
+const getClaimsController = async (res, id, variant) => {
+  // Validate request
+
+  // Access database
+  const sql = buildClaimsSelectSql(id, variant);
+  const { isSuccess, result, message } = await read(sql);
+  if (!isSuccess) return res.status(400).json({ message });
+  // Response to request
+  res.status(200).json(result);
+};
+
+// Endpoints ------------------------------
+// Claims
+app.get("/api/claims", (req, res) => getClaimsController(res, null, null));
+app.get("/api/claims/:id", (req, res) =>
+  getClaimsController(res, req.params.id, null)
+);
+app.get("/api/claims/users/:id", (req, res) =>
+  getClaimsController(res, req.params.id, "users")
+);
+
+// Start server ----------------------------
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
