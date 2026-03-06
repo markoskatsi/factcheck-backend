8d298102c484319f4f99bda0f72d086208347e71
Markos Katsi <markoskatsi05@gmail.com>
2026-03-06 17:58:39 +0000

    git fetch reset

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 docker stop factcheck-container
 docker rm factcheck-container
-git pull origin main
+git fetch origin main
+git reset --hard origin/main
 docker build -t factcheck-backend .
 docker run -d --name factcheck-container --restart unless-stopped --env-file .env -p 3000:5000 factcheck-backend
 echo "Deployment complete"
