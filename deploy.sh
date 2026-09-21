87b6f820909bb2451cfba7d5726db491df5f22f3
markoskatsi <markoskatsi05@gmail.com>
2026-09-21 14:10:00 +0100

    deploy script to use git pull and cached build

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+git pull origin main
+docker build -t factcheck-backend .
 docker stop factcheck-container
 docker rm factcheck-container
-git fetch origin main
-git reset --hard origin/main
-docker build --no-cache -t factcheck-backend .
 docker run -d --name factcheck-container --restart unless-stopped --env-file .env -p 3000:5000 factcheck-backend
 echo "Deployment complete"
